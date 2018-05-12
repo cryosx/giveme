388792ce4305bcd94f40b70fca0ca9ef4be616aa
cryosx <cyruswu.email@gmail.com>
2018-05-12 09:33:14 -1000

    knex setup

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    debug: true,
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'giveme_user',
+      password: 'giveme_user_password',
+      database: 'giveme',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: path.join(__dirname, 'db/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db/seeds')
+    }
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
